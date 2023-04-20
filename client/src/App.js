3497b92683cab0c417e683d5606e0928554c0cbd
MiguelGuevara <42946893+MiguelGuevaraAvila@users.noreply.github.com>
2023-04-20 18:21:59 -0500

    Front and Back
    Comunication form to api back and insert into database.
    
    Get list of Books reviews

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Axios from 'axios';
 
+
 function App() {
   const [bookName, setBookName ]= useState("");
-  const [review, setReview]= useState("");
+  const [bookReview, setReview]= useState("");
+  const [bookReviewList, setBookList] = useState([]);
   
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/api/get").then((response)=>{
+      setBookList(response.data);
+    });
+  },[]);
+
   const submitReview = ()=>{
-    Axios.post('http://localhost:3001',{
-      bookName:bookName,
-      bookReview:review,
+    Axios.post('http://localhost:3001/api/insert',{
+      'bookName':bookName,
+      'bookReview':bookReview
     }).then(()=>{
       alert('Review successfully submitted');
     })
@@ -21,17 +29,33 @@ function App() {
     <div className="App">
       <h1>CRUD Application APP</h1>
 
-      <div className="form">
+      <div className="form" method='POST'>
         <label for="bookName">Book name:</label>
           <input type="text" name="bookName"  onChange={(e)=>{
           setBookName(e.target.value);
         }}/>
         <label for="bookName">Review:</label>
-          <input type="text" name="review" onChange={(e)=>{
+          <input type="text" name="bookReview" onChange={(e)=>{
           setReview(e.target.value);
         }}/>
 
           <button onClick={submitReview}>Submit</button>
+
+          {
+            bookReviewList.map((val)=>{
+              return (
+                <div>
+                    <h3>Nombre del Libro :     
+                      <span>
+                        {val.bookName} 
+                      </span>
+                    </h3>
+                    <p>
+                      Opiniones del libro : {val.bookReview}
+                    </p>
+                </div>)
+            })
+          }
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors')
 
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'toor',
-    database: 'CRUDDataBase',
+    database: 'cruddatabase',
     port:3306
 });
 
+/*
+db.getConnection(function (err, connection) {
+    console.log("connected");
+})*/
 
 app.use(cors());
 app.use(express.json());
@@ -19,19 +23,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/get', (req, res) => {
     const  sqlSelect ="SELECT * FROM book_reviews"
+
     db.query(sqlSelect,(err, result)=>{
-        console.log(result);
+        try {
+            res.send(result);
+        } catch (error) {
+            console.log(err);
+        }
     })
 })
-
 
 app.post('/api/insert', (req, res) => {
     const bookName = req.body.bookName;
     const bookReview = req.body.bookReview;
 
-    const sqlInsert = "INSERT INTO book_reviews (bookName, bookReview)  VALUES (?,?)";
-    db.query(sqlInsert,[bookName, bookReview],(err, result) => {
-        console.log(err); 
+    const sqlInsert = "INSERT INTO book_reviews(bookName, bookReview)  VALUES (?,?)";
+    db.query(sqlInsert,[bookName, bookReview], (err, result) => {
+        try {
+            console.log(result);
+        } catch (error) {
+            console.log(err);
+        }
     });
 });
 
